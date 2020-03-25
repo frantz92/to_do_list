f4677accd9bde45bd352dd56a1bca30ef343c54b
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-03-25 19:11:34 +0100

    Repair icons

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -35,9 +35,7 @@ class Column extends React.Component {
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.title}
         <span className={styles.icon}>
-          {this.state.columns.map(({key, title, icon, cards}) => (
-            <Icon name={icon} />
-          ))}
+            <Icon name={this.props.icon} />
         </span>
         </h3>
         <div>
