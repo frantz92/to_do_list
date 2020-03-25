95f9945be1615ce5261e7136b223db49bb2e95b2
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-03-25 11:36:35 +0100

    Change code to use data from dataStore.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
+
   static propTypes ={
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
     image: PropTypes.string,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>
+    description: settings.defaultListDescription,
   }
 
   render() {
@@ -20,12 +27,12 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageSrc={this.props.image}/>
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-          <Column title={'Animals'} />
-          <Column title={'Plants'} />
-          <Column title={'Minerals'} />
+          {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
       </section>
     )
