0c722838d0aae1102c48837f217fb48e81fdfcb3
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-03-30 15:02:09 +0200

    Add generate new links while search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styles from './Header.scss';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 import {pageContents} from '../../data/dataStore';
+import Search from '../Search/SearchContainer';
 
 class Header extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class Header extends React.Component {
             <Link className={styles.logo} to='/'>
               <Icon name={pageContents.icon} />
             </Link>
+            <Search />
             <nav>
               <NavLink exact to='/' activeClassName='active'>Home</NavLink>
               <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
